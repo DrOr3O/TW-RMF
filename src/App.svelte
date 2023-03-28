6485c13385eac3a78186d20c000a86ec07e14353
Jeff Lin <rslin@ucsd.edu>
2023-03-28 16:26:47 -0700

    add layout component

--- src/App.svelte
+++ src/App.svelte
@@ -1,2 +1,10 @@
 <script lang="ts">
+    import Layout from './Layout.svelte'
 </script>
+
+<Layout>
+  <p>your mom.</p>
+</Layout>
+
+<style>
+</style>
---
+++ src/Layout.svelte
@@ -0,0 +1,20 @@
+<body>
+    <header />
+    <main>
+        <slot />
+    </main>
+    <footer>
+        <p>© 2023 Not Safe For School. All rights reserved.</p>
+    </footer>
+</body>
+
+<style>
+    body {
+        display: flex;
+        flex-direction: column;
+    }
+
+    footer {
+        margin-top: auto;
+    }
+</style>
