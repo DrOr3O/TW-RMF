9c7dfd1e40dacb9c3aa57aea10912446dc5c2455
davidtso1219 <davidtso1219@gmail.com>
2023-03-29 12:16:02 -0700

    Added Frontend Search Bar Functionality
    TODO:
    1. Create a component for each professor in search result
    2. Finish query functionality on backend

--- src/pages/HomePage.svelte
+++ src/pages/HomePage.svelte
@@ -1,5 +1,47 @@
 <script lang="ts">
   import Layout from "../layouts/Layout.svelte";
+
+  let query = "";
+  let professors: Professor[] = [];
+
+  const useTimer = (cb: Function, ms: number = 1000) => {
+    let timer: number | undefined;
+
+    return () => {
+      if (timer) clearTimeout(timer);
+      timer = setTimeout(cb, ms);
+    }
+  }
+
+  const showSearchResult = useTimer(async () => {
+    professors = query === "" ? [] : await getProfessors(query);
+  }, 500);
+
+  const getProfessors = async (query: string): Promise<Professor[]> => {
+    const url = `/api/professors?query=${query}`;
+    const options = {
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    }
+    let res = await fetch(url, options);
+    let json = await res.json();
+    return json.data;
+  }
 </script>
 
-<Layout />
+<Layout>
+  <!-- TODO: form fallack for no-JS users -->
+  <form>
+    <input type="text" name="query" id="query" bind:value={query} on:input={showSearchResult}/>
+  </form>
+  <output>
+    {#if professors.length === 0}
+      <p>Please enter something</p>
+    {:else}
+      {#each professors as prof}
+        <p>{prof.name}</p>
+      {/each}
+    {/if}
+  </output>
+</Layout>
